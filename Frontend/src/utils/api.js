112671831b4e7eb424394f72c1d64453ba5ef989
Gunjan <Gunjan.16299@stu.upes.ac.in>
2026-01-12 06:39:49 +0530

    Update api.js

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://ecommerce-scraper-production.up.railway.app/api";
 
 export const api = {
   // ✅ Start scraping (returns session_id)
